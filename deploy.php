a30a45709e591436e77a2880538997633995e9dc
guilhem <guilhem.bonnetjalabert@gmail.com>
2023-07-31 14:04:24 +0200

    d

--- deploy.php
+++ deploy.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $secret = "cfJzk5^Gjq6jw&yW"; // Créez un secret pour sécuriser votre webhook
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 
